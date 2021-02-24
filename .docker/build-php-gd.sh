13cbaecaa4951d2d5afe1ce3e100a3fe6fcb24e8
Denis Kostin <kd@kodix.ru>
2021-02-24 12:34:07 +0300

    add support gd for php 7.4

---
+++ .docker/build-php-gd.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+echo "START CONFIGURING php-gd"
+
+VERSION=`echo $PHP_VERSION | grep "^7\.4"`
+
+apk add libgd zlib-dev \
+    freetype freetype-dev \
+    libpng libpng-dev \
+    libjpeg-turbo libjpeg-turbo-dev \
+    libwebp libwebp-dev
+
+if [ ! -z  $VERSION ]
+then
+  docker-php-ext-configure gd \
+    --with-freetype \
+    --with-jpeg \
+    --with-webp
+else
+  docker-php-ext-configure gd \
+    --with-freetype-dir=/usr/include/freetype2 \
+    --with-png-dir=/usr/include \
+    --with-jpeg-dir=/usr/include \
+    --with-webp-dir=/usr/include
+fi
+
+apk del zlib-dev freetype-dev libpng-dev libjpeg-turbo-dev libwebp-dev
+
+echo "END OF CONFIGURING php-gd"
